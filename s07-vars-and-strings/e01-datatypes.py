824dca9e6d3340788cde4d545abd9774bfbae177
ngrosso <nzgrosso@gmail.com>
2019-04-22 23:36:20 -0300

    primer dia de clases de udemy

---
+++ s07-vars-and-strings/e01-datatypes.py
@@ -0,0 +1,25 @@
+#S7C45
+#Data types:
+
+bool #Boolean True False
+int #Integer
+str #String unicode
+list #List [1,2,3] ["A","B","C"]
+dict #Dictionary key value pair {"name":"Zephi", "surname":"Nyan"}
+
+some_string = "8"
+type(some_string) #Output: class 'str'
+
+#S7C46
+#Dynamic Typing:
+
+#vars can change types
+a= True #bool
+a= "Dog" #str
+a= 22/7 #Output: 3.1423232 float
+
+#S7C47
+#None -> nada (null) sirve como inicializador en caso de que no haya nada
+
+#S7C48
+#Double vs single quotes ( "asd" vs 'asd' )
